03484fd116aadf4fbd9c86b2410c6d4f21076346
HK <140630172+Kiro111@users.noreply.github.com>
2023-07-30 20:02:56 +0300

    1.0.3

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -3,8 +3,16 @@ using System.Collections;
 using Cinemachine;
 using System;
 
+[RequireComponent(typeof(Rigidbody), typeof(BoxCollider))] 
+
 public class PlayerManager : MonoBehaviour
+
 {
+    [SerializeField] private Canvas canvas;
+    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private DynamicJoystick joystick;
+    private bool isJoystickActive = false;
+   
     public float moveSpeed = 5f; // Скорость перемещения стикмена
     private Vector2 lastDirection = Vector2.zero;
     public GameObject bulletPrefab; // Префаб для шарика
@@ -55,6 +63,9 @@ public class PlayerManager : MonoBehaviour
             virtualCamera.LookAt = transform;
         }
 
+        //HandleJoystick();
+
+
         if (isAiming && aimLineRenderer != null)
         {
             RaycastHit hit;
@@ -78,11 +89,11 @@ public class PlayerManager : MonoBehaviour
             // Начало свайпа
             if (touch.phase == TouchPhase.Began)
             {
-                inputPosition = touch.position; 
-    
-                    isMoving = true;
+                inputPosition = touch.position;
+
+                isMoving = true;
                 FixedUpdate();
-               
+
 
 
 
@@ -91,7 +102,7 @@ public class PlayerManager : MonoBehaviour
             else if (touch.phase == TouchPhase.Ended)
             {
                 isMoving = false;
-               
+                playerAnimator.SetBool("Walking", true);
             }
         }
         // Обработка перемещения мыши
@@ -100,7 +111,7 @@ public class PlayerManager : MonoBehaviour
             inputPosition = Input.mousePosition;
             isMoving = true;
             FixedUpdate();
-           
+
 
 
 
@@ -108,69 +119,64 @@ public class PlayerManager : MonoBehaviour
         else
         {
             isMoving = false;
-           
+            playerAnimator.SetBool("Walking", true);
         }
 
         // Устанавливаем параметр "Walking" в Animator в зависимости от состояния движения
         if (isMoving && isShooting)
-        {
-            playerAnimator.SetBool("Shoot", true);
-            
+            {
+                playerAnimator.SetBool("Shoot", true);
 
-        }
-        if (isMoving)
-        {
-            playerAnimator.SetBool("Walking", true);
-            
 
-        }
-        else
-        {
-            playerAnimator.SetBool("Walking", false);
-        }
+            }
+            if (isMoving)
+            {
+                playerAnimator.SetBool("Walking", true);
+
+
+            }
+            else
+            {
+                playerAnimator.SetBool("Walking", false);
+            }
 
         // Плавный поворот стикмена в сторону движения
-        if (isMoving)
-        {
-            Vector3 movementDirection = new Vector3(inputPosition.x - Screen.width / 2f, 0f, inputPosition.y - Screen.height / 2f).normalized;
-            Quaternion targetRotation = Quaternion.LookRotation(movementDirection);
-            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * 20f);
-        }
-
+  
         // Проверяем, есть ли враги в радиусе обнаружения и стреляем на них
-        Collider[] hitColliders = new Collider[10]; // Максимальное количество коллайдеров для обнаружения
-        int numColliders = Physics.OverlapSphereNonAlloc(transform.position, detectionRadius, hitColliders);
-        Transform closestEnemy = null;
-        float closestDistanceSqr = Mathf.Infinity;
+        Collider[] hitColliders = new Collider[1]; // Максимальное количество коллайдеров для обнаружения
+            int numColliders = Physics.OverlapSphereNonAlloc(transform.position, detectionRadius, hitColliders);
+            Transform closestEnemy = null;
+            float closestDistanceSqr = Mathf.Infinity;
 
-        for (int i = 0; i < numColliders; i++)
-        {
-            if (hitColliders[i].CompareTag(enemyTag))
+            for (int i = 0; i < numColliders; i++)
             {
-                // Находим ближайшего врага
-                Transform enemyTransform = hitColliders[i].transform;
-                float distanceSqr = (enemyTransform.position - transform.position).sqrMagnitude;
-                if (distanceSqr < closestDistanceSqr)
+                if (hitColliders[i].CompareTag(enemyTag))
                 {
-                    closestDistanceSqr = distanceSqr;
-                    closestEnemy = enemyTransform;
+                    // Находим ближайшего врага
+                    Transform enemyTransform = hitColliders[i].transform;
+                    float distanceSqr = (enemyTransform.position - transform.position).sqrMagnitude;
+                    if (distanceSqr < closestDistanceSqr)
+                    {
+                        closestDistanceSqr = distanceSqr;
+                        closestEnemy = enemyTransform;
+                    }
                 }
             }
-        }
 
         // Если есть ближайший враг, стреляем на него
         if (closestEnemy != null)
         {
             // Поворачиваемся в сторону врага
             Vector3 directionToEnemy = closestEnemy.position - transform.position;
-            directionToEnemy.y = 0f;
-            Quaternion targetRotation = Quaternion.LookRotation(directionToEnemy);
-            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * 40f);
+            directionToEnemy.y = 0f; // Оставляем только горизонтальное направление
+            Quaternion targetRotation1 = Quaternion.LookRotation(directionToEnemy);
+
+            // Применяем поворот без плавной интерполяции
+            transform.rotation = targetRotation1;
 
             // Прицеливаемся на врага
             isAiming = true;
-
-         
+            playerAnimator.SetBool("Shoot", true);
 
             // Проверяем, прошла ли задержка между выстрелами
             if (Time.time >= lastShootTime + shootCooldown)
@@ -179,78 +185,151 @@ public class PlayerManager : MonoBehaviour
                 canShoot = true;
             }
         }
+
         else
         {
-            // Если врагов в радиусе обнаружения нет, сбрасываем прицеливание и возможность стрельбы
-            isAiming = false;
-            canShoot = false;
-            playerAnimator.SetBool("Shoot", false);
+                // Если врагов в радиусе обнаружения нет, сбрасываем прицеливание и возможность стрельбы
+                isAiming = false;
+                canShoot = false;
+                playerAnimator.SetBool("Shoot", false);
         }
 
-        // Проверяем, может ли игрок прицелиться и стрелять
-        canAimAndShoot = isAiming && canShoot;
+            // Проверяем, может ли игрок прицелиться и стрелять
+            canAimAndShoot = isAiming && canShoot;
 
-        // Если игрок прицеливается и может стрелять, то стреляем
-        if (canAimAndShoot)
-        {
-            ShootBullet();
-            isShooting = true;
-        }
-
-        // Отображаем луч прицеливания, если есть LineRenderer
-        if (aimLineRenderer != null)
-        {
-            if (isAiming)
+            // Если игрок прицеливается и может стрелять, то стреляем
+            if (canAimAndShoot)
             {
-                aimLineRenderer.enabled = true;
-                aimLineRenderer.SetPosition(0, firePoint.position);
-                aimLineRenderer.SetPosition(1, firePoint.position + firePoint.forward * detectionRadius);
+                ShootBullet();
+                isShooting = true;
             }
-            else
+
+            // Отображаем луч прицеливания, если есть LineRenderer
+            if (aimLineRenderer != null)
             {
-                aimLineRenderer.enabled = false;
+                if (isAiming)
+                {
+                    aimLineRenderer.enabled = true;
+                    aimLineRenderer.SetPosition(0, firePoint.position);
+                    aimLineRenderer.SetPosition(1, firePoint.position + firePoint.forward * detectionRadius);
+                }
+                else
+                {
+                    aimLineRenderer.enabled = false;
+                }
+                //}
             }
-        }
     }
 
-    private void StartMoving(int v)
-    {
-        throw new NotImplementedException();
-    }
+    //private void StartMoving(int v)
+    //{
+    //    throw new NotImplementedException();
+    //}
 
 
+    //private void HandleJoystick()
+    //{
+    //    // Если на экране есть касания
+    //    if (Input.touchCount > 0)
+    //    {
+    //        Touch touch = Input.GetTouch(0);
+
+    //        // Если началось касание, активируем джойстик и устанавливаем его позицию
+    //        if (touch.phase == TouchPhase.Began)
+    //        {
+    //            inputPosition = touch.position;
+    //            isJoystickActive = true;
+
+    //            // Переводим позицию касания в локальные координаты Canvas
+    //            RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.transform as RectTransform, inputPosition, canvas.worldCamera, out Vector2 localPosition);
+
+    //            // Устанавливаем позицию джойстика в Canvas
+    //            joystick.transform.localPosition = localPosition;
+    //        }
+    //        // Если касание закончилось, деактивируем джойстик
+    //        else if (touch.phase == TouchPhase.Ended)
+    //        {
+    //            isJoystickActive = false;
+    //            playerAnimator.SetBool("Walking", true);
+    //            joystick.gameObject.SetActive(false); // Отключаем джойстик после окончания касания
+    //        }
+    //    }
+    //    // Если есть щелчок мыши, обрабатываем активацию джойстика для тестирования на ПК
+    //    else if (Input.GetMouseButtonDown(0))
+    //    {
+    //        inputPosition = Input.mousePosition;
+    //        isJoystickActive = true;
+
+    //        // Переводим позицию касания в локальные координаты Canvas
+    //        RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.transform as RectTransform, inputPosition, canvas.worldCamera, out Vector2 localPosition);
+
+    //        // Устанавливаем позицию джойстика в Canvas
+    //        joystick.transform.localPosition = localPosition;
+    //    }
+
+    //    // Включаем или отключаем джойстик в соответствии с его состоянием
+    //    joystick.gameObject.SetActive(isJoystickActive);
+
+    //    // Если джойстик активен, двигаем игрока
+    //    if (isJoystickActive)
+    //    {
+    //        FixedUpdate();
+    //    }
+    //}
+
+
+
+
+
+
+
+    private Quaternion targetRotation; // Добавляем новую переменную для хранения целевого поворота
 
    
 
     void FixedUpdate()
     {
-        if (Input.touchCount > 0)
+        _rigidbody.velocity = new Vector3(joystick.Horizontal * moveSpeed, _rigidbody.velocity.y, joystick.Vertical * moveSpeed);
+
+        if (joystick.Horizontal != 0 || joystick.Vertical != 0)
         {
-            Vector2 currentInputPosition = Input.GetTouch(0).position;
-            // Вычисление направления движения
-            Vector2 direction = currentInputPosition - inputPosition;
-
-            // Нормализация направления движения
-            direction.Normalize();
-
-            // Применение силы для перемещения стикмена
-            Vector3 movement = new Vector3(direction.x, 0f, direction.y) * moveSpeed * Time.fixedDeltaTime;
-            transform.Translate(movement, Space.World);
-
-            inputPosition = currentInputPosition;
-            lastDirection = direction;
+            Vector3 movementDirection = new Vector3(joystick.Horizontal, 0f, joystick.Vertical).normalized;
+            if (movementDirection != Vector3.zero)
+            {
+                targetRotation = Quaternion.LookRotation(movementDirection);
+            }
         }
-        //else
-        //{
-        //    // Если нет активных касаний, двигаем стикмена в последнем запомненном направлении
-        //    Vector3 movement = new Vector3(lastDirection.x, 0f, lastDirection.y) * moveSpeed * Time.fixedDeltaTime;
-        //    transform.Translate(movement, Space.World);
-           
-        //}
-       
+
+        // Применяем целевой поворот без плавной интерполяции
+        transform.rotation = targetRotation;
     }
+    //    if (Input.touchCount > 0)
+    //    {
+    //        Vector2 currentInputPosition = Input.GetTouch(0).position;
+    //        // Вычисление направления движения
+    //        Vector2 direction = currentInputPosition - inputPosition;
+
+    //        // Нормализация направления движения
+    //        direction.Normalize();
+
+    //        // Применение силы для перемещения стикмена
+    //        Vector3 movement = new Vector3(direction.x, 0f, direction.y) * moveSpeed * Time.fixedDeltaTime;
+    //        transform.Translate(movement, Space.World);
+
+    //        inputPosition = currentInputPosition;
+    //        lastDirection = direction;
+    //    }
+    //    else
+    //    {
+    //        // Если нет активных касаний, двигаем стикмена в последнем запомненном направлении
+    //        Vector3 movement = new Vector3(lastDirection.x, 0f, lastDirection.y) * moveSpeed * Time.fixedDeltaTime;
+    //        transform.Translate(movement, Space.World);
+
+    //    }
+
+
     //Добавив инициализацию inputPosition, код должен правильно сохранять позицию касания и использовать ее для вычисления направления движения.Теперь ваш стикмен должен двигаться в последнем запомненном направлении, когда на экране нет активных касаний.
-    
+
 
 
 
@@ -258,7 +337,7 @@ public class PlayerManager : MonoBehaviour
 
 
     // Отрисовываем радиус обнаружения для наглядности в редакторе Unity
-        void OnDrawGizmosSelected()
+    void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, detectionRadius);
